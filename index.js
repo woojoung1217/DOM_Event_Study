e6269049daa73694d9111a45b97eb05c4dc9c5ac
woojoung <ywj981217@naver.com>
2023-05-01 01:34:58 +0900

    study domevent

--- index.js
+++ index.js
@@ -26,25 +26,69 @@
 
 //parentNode =>
 
-const red = document.getElementById("red");
-let rep = red.parentNode;
-// 부모요소 전체를 반환
-let rep2 = red.parentElement;
-// 부모요소 노드의 속성을 반환
-console.log(rep);
-console.log(rep2);
-// 자식 요소 찾는법
-const ulel = document.getElementById("color");
+// const red = document.getElementById("red");
+// let rep = red.parentNode;
+// // 부모요소 전체를 반환
+// let rep2 = red.parentElement;
+// // 부모요소 노드의 속성을 반환
+// console.log(rep);
+// console.log(rep2);
+// // 자식 요소 찾는법
+// const ulel = document.getElementById("color");
 
-let ulchild = ulel.childNodes;
-// 반환 수가 다름 : text(공백) 요소  까지 반환
-let ulchildren = ulel.children;
-console.log(ulchild);
-//
-console.log(ulchildren);
-// HTML collection 의 경우 실시간의 변경 값을 반영
-console.log(ulel.firstElementChild);
-console.log(ulel.lastElementChild);
+// let ulchild = ulel.childNodes;
+// // 반환 수가 다름 : text(공백) 요소  까지 반환
+// let ulchildren = ulel.children;
+// console.log(ulchild);
+// //
+// console.log(ulchildren);
+// // HTML collection 의 경우 실시간의 변경 값을 반영
+// console.log(ulel.firstElementChild);
+// console.log(ulel.lastElementChild);
 
-const blue = document.getElementById("blue");
-console.log(blue.previousElementSibling);
+// const blue = document.getElementById("blue");
+// console.log(blue.previousElementSibling);
+// 생성
+
+// const newEl = document.createElement("li");
+// newEl.innerHTML = " black";
+// const ul = document.getElementById("color");
+// ul.appendChild(newEl);
+
+// //
+
+// const newEl2 = document.createElement("li");
+// const newText = document.createTextNode("pink");
+// newEl2.appendChild(newText);
+// ul.appendChild(newEl2);
+
+// // insertBefore
+
+// const newEl3 = document.createElement("li");
+// const newText2 = document.createTextNode("yellow");
+
+// newEl3.appendChild(newText2);
+// const red = document.getElementById("red");
+// ul.insertBefore(newEl3, red);
+
+// css , style 적용
+
+// const box = document.getElementById("Box");
+// // box.style.backgroundColor = "red";
+// // box.style.color = "#fff";
+// // box.style.width = "100px";
+// // box.style.height = "100px";
+// // box.style.border = "10px solid #ddd";
+
+// box.classList.add("txt-pink");
+
+// const color = document.getElementById("color");
+// color.onclick = function (e) {
+//   const target = e.target;
+//   console.log(target);
+//   if (target.tagName !== "LI") {
+//     return;
+//   } else {
+//     target.classList.toggle("txt-pink");
+//   }
+// };
